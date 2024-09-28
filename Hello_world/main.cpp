68061d72bde476053e16d3a78beaa62a515412f2
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-09-28 11:51:58 +0330

    constants

--- Hello_world/main.cpp
+++ Hello_world/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
-int main() {
-    int a = 1;
-    int b = 2;
-    int temp = 0;
-    temp = a;
-    a = b;
-    b = temp;
-    std::cout << a;
+// int main() {
+//     int a = 1;
+//     int b = 2;
+//     int temp = 0;
+//     temp = a;
+//     a = b;
+//     b = temp;
+//     std::cout << a;
+//     return 0;
+// }
+
+int main(){
+    const double pi = 3.14;
     return 0;
 }
